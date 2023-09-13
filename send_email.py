9a7ad9f5536b819fede68fdbe8aa6a1d3bd07bfd
nursultan-berdiev <dominguez120205024@gmail.com>
2023-09-13 11:45:03 +0600

    added addresses to send message

--- send_email.py
+++ send_email.py
@@ -1,3 +1,8 @@
 def send_mail(email_text, address, title):
     message = email_text.format()
     return message + address + title
+
+
+def get_addresses(filepath):
+    with open(filepath, 'r') as file:
+        return file.read()
